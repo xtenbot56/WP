c15584d045760a7e53efa7a2cf00fc7b322b7a2a
xtenbot56 <wpbot116@gmail.com>
2025-04-27 00:07:37 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Hussainbot',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '91702395151X', //Set your number here without + symbol, just add country code & number without any space
+  botName: "HUSSAIN XT",
+  botOwner: 'HUSSAIN XT', // Your name
+  ownerNumber: '01965142856', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
