ec9918cccd77aea82eea991b19ca40af15b172b4
𝘴น𝚖𝔞ꪦ_𝗿ǿⲩ <panelbot10@gmail.com>
2025-05-29 10:26:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "923003588997",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "917003816486",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "KAISEN",
 // add bot owner name 
@@ -67,7 +67,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "923003588997",
+DEV: process.env.DEV || "917003816486",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
